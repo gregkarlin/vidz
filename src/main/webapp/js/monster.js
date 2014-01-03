dd4d8b1832515a61932a9d51ffd804b070631baf
Greg Karlin <gregory.l.karlin@gmail.com>
2014-01-03 11:50:36 -0500

    added earth and arms

--- src/main/webapp/js/monster.js
+++ src/main/webapp/js/monster.js
@@ -21,34 +21,30 @@ var test_arms = {
   "third":[1,2,3],
   "fourth":[1,2,3],
   "fifth":[1,2,3],
-  "sixth":[1,2,3],
-  "seventh":[1,2,3],
-  "eigth":[1,2,3],
-  "nine":[1,2,3],
-  "ten":[1,2,3],
-  "eleven":[1,2,3],
-  "twelve":[1,2,3]
+  "sixth":[1,2,3]
 };
 
 var keys = Object.keys(test_arms);
 
 var length = keys.length;
 
-var index = 0;
-var rect_width = 80;
-var rect_height = 10;
+var index = 1;
+var rect_width = 210;
+var rect_height = 180;
 var initial_x_offset = $(window).width()/2 + rect_width/2;
 var initial_y_offset = $(window).height()/2 + rect_height/2;
-
 Object.keys(test_arms).map(function(key) {
-  canvas.append("rect")
+  canvas.append('svg:image')
+    .attr("xlink:href",'../images/arm_small.gif')
     .attr("width",rect_width)
     .attr("height",rect_height)
-    .attr("transform","translate(" + initial_x_offset + "," + initial_y_offset + ") rotate(" + (index*360/length) + ")")
-    //.attr("transform","rotate(" + (index * 360 / length) + "," + height/2 + "," + width/2 + ")")
-    //.attr("transform","rotate(10,100,200)");
-  index++;
-  console.log(index)
+    .attr("transform","rotate(" + (index*-360/length) + ",100,220)") 
+    .on("mouseover",function(){alert("fuck");});
+    //.attr("transform","translate(" + initial_x_offset + "," + initial_y_offset + ") rotate(" + (index*-360/length) + ")");
+    index++;
 });
-
-
+  canvas.append('svg:image')
+    .attr("xlink:href",'../images/jupiter.gif')
+    .attr("width",200)
+    .attr("height",200)
+    .attr("transform","translate(0,120)");
